e9ec8cd8d8d35546951f706c91fdc587beb4e302
JRC-Tech <javier.ruzzante@gmail.com>
2020-10-22 02:41:56 -0400

    Major bug fixes on routing...

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path =require('path');
 const fs = require('fs');
 const mediaserver = require('mediaserver');
 const multer = require('multer');
+const { data } = require('jquery');
 
 var opcionesMulter = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -16,72 +17,72 @@ var opcionesMulter = multer.diskStorage({
 
 var upload = multer({storage: opcionesMulter});
 
-app.set('rutaCancionesActual', 'canciones');
 app.use(express.static('public'));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
 app.get('/', function(req, res){
-    app.set('rutaCancionesActual', 'canciones');
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/subir', function(req, res){
-    if (path.parse(app.get('rutaCancionesActual')).dir == '')
-        app.set('rutaCancionesActual', 'canciones')
-    else
-        app.set('rutaCancionesActual', path.parse(app.get('rutaCancionesActual')).dir);
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+var canciones = [];
+var fileNumber;
 app.get('/canciones', apiCanciones);
 
 function apiCanciones(req,res) {
-    console.log(path.parse(app.get('rutaCancionesActual')));
     console.log('New ' + req.method + ' Host: ' + req.headers.host);
     //Actualiza la lista de archivos en el archivo canciones.json
-    fs.readdir(path.join(__dirname, app.get('rutaCancionesActual')), function(err, archivos){
-        if (err) throw err;
-        var canciones = JSON.parse("[]");
-        archivos.forEach(function(nombre){
-            const file = path.join(__dirname, app.get('rutaCancionesActual'), nombre);
-            var dataFile =null;
-            try{
-                dataFile =fs.lstatSync(file);
-            }catch(e){}
-            canciones.push({"nombre": nombre, "isDir": dataFile.isDirectory(), "path": app.get('rutaCancionesActual'), "extension": path.extname(file)});
-        });
-        //console.log(canciones);
-        fs.writeFile(path.join(__dirname, 'canciones.json'), JSON.stringify(canciones), function(err){
-            if (err) throw err;
-            //Lee el archivo canciones.json
-            fs.readFile(path.join(__dirname, 'canciones.json'), 'utf8', function(err, canciones){
-                if(err) throw err;
-                res.json(JSON.parse(canciones));
-            });
-        });
+    canciones = [];
+    fileNumber = 0;
+    scanDirs(path.join(__dirname, 'canciones'));
+    const jsonString = JSON.stringify(canciones);
 
+    fs.writeFile('./canciones.json', jsonString, function(err){
+        if (err) {
+            console.log('Error al escribir en el archivo', err)
+        } else {
+            res.json(canciones);
+        }
     });
-
     
 }
-
-app.get('/canciones/:nombre', function(req, res) {
-    
-    var file = path.join(__dirname, app.get('rutaCancionesActual'), req.params.nombre);
-    var dataFile = null;
+function scanDirs(directoryPath){
     try{
-        var dataFile =fs.lstatSync(file);
+       var ls=fs.readdirSync(directoryPath);
+       for (let index = 0; index < ls.length; index++) {
+          const file = path.join(directoryPath, ls[index]);
+          var dataFile =null;
+          try{
+             dataFile =fs.lstatSync(file);
+          }catch(e){}
+ 
+          if(dataFile){
+             canciones.push(
+                {
+                   fileIndex: fileNumber, 
+                   path: path.parse(file.replace(__dirname, '')).dir,
+                   nombre: ls[index],
+                   isDir: dataFile.isDirectory(),
+                   extension: path.extname(ls[index])
+                });
+             fileNumber++;
+ 
+             if(dataFile.isDirectory()){
+                scanDirs(file)
+             }
+          }
+       }
     }catch(e){}
+ }
+
+app.get('/canciones/:fileIndex', function(req, res) {
+
+    nReqFile = req.params.fileIndex;
+    
+    var file = path.join(__dirname, canciones[nReqFile].path, canciones[nReqFile].nombre);
+    
     try{
-        if (dataFile.isDirectory()){
-            console.log('Requested folder: ', req.params.nombre);
-            app.set('rutaCancionesActual', path.join(app.get('rutaCancionesActual'), req.params.nombre));
-            apiCanciones(req, res);
-        }
-        else{
-            console.log('Requested: ', req.params.nombre);
-            mediaserver.pipe(req, res, file);
-        }
+        console.log('Requested: ', file);
+        mediaserver.pipe(req, res, file);
     } catch(e){console.log('Error:', file);}
     
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+var rutaActual = '\\canciones';
 var arrayCanciones;
 var arrayCarpetas;
 var indice = 0;
@@ -14,7 +15,7 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
         if (arrayCanciones[indice] != undefined){
             //console.log(indice);
             audio[0].pause();
-            audio.attr('src', '/canciones/' + arrayCanciones[indice].nombre);
+            audio.attr('src', '/canciones/' + arrayCanciones[indice].fileIndex);
             document.getElementsByClassName('cancion-actual')[0].innerHTML = '<marquee>' + arrayCanciones[indice].nombre + '</marquee>';
             audio[0].play();
             elemActual = document.getElementsByClassName('cancion')[indice];
@@ -27,9 +28,9 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
         }
     });
     
-    function cargarCanciones (folder) {
+    function cargarCanciones () {
         $.ajax({
-            url: '/canciones/' + folder
+            url: '/canciones/'
         }).done(function (canciones) {
             arrayCanciones = [];
             arrayCarpetas = [];
@@ -40,23 +41,21 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
             lista.empty();
             listaCarpetas.empty();
 
-            if(canciones[0].path != 'canciones'){
+            if(rutaActual != '\\canciones'){
                 $('<li class = subir><i class="fa-li fas fa-arrow-up"></i></i>Subir...</li>')
                 .on('click', subir)
                 .appendTo(listaCarpetas);
             }
-
-            //$('.lbl-carpetas')[0].innerHTML = '<i class="far fa-folder-open">   '+folder;
             var i=0;
             canciones.forEach(function (cancion){
-                if (cancion.isDir){
+                if ((cancion.isDir) && cancion.path == rutaActual){
                     var nuevaCarpeta = $('<li class = carpeta><i class="fa-li far fa-folder"></i>' + cancion.nombre + '</li>');
                     nuevaCarpeta
                         .on('click', cancion, cambiarCarpeta)
                         .appendTo(listaCarpetas);
                     arrayCarpetas.push(cancion.nombre);
                 }
-                else if(cancion.extension == '.mp3'){
+                else if((cancion.extension == '.mp3') && (cancion.path == rutaActual)){
                     var nuevoElemento = $('<li indice = "' + i + '" class = "cancion">'+ cancion.nombre + '</li>');
                     nuevoElemento
                         .on('click', cancion, play)
@@ -70,12 +69,12 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
             }
 
             if (arrayCanciones.length != 0){
-                audio.attr('src','/canciones/' + arrayCanciones[0].nombre);
+                audio.attr('src', '/canciones/' + arrayCanciones[indice].fileIndex);
                 elemActual = document.getElementsByClassName('cancion')[0];
                 elemActual.style.color = "#4ad3dd";
                 document.getElementsByClassName('cancion-actual')[0].innerHTML = '<marquee>' + arrayCanciones[0].nombre + '</marquee>';
             }
-            $('.lbl-carpeta')[0].innerHTML = '<i class="far fa-folder-open">   '+canciones[0].path;
+            $('.lbl-carpeta')[0].innerHTML = '<i class="far fa-folder-open">   '+rutaActual;
                 
             }).fail(function(){
                 alert('No pude cargar las canciones');
@@ -89,7 +88,9 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
             elemActual.style.color = "#4ad3dd";
             //console.log(evento.currentTarget.attributes);
             audio[0].pause();  //Se refiere al objeto audio nativo del DOM
-            audio.attr('src', '/canciones/' + evento.data.nombre);
+            //audio.attr('src', '/canciones/' + evento.data.nombre);
+            console.log(evento.data.fileIndex);
+            audio.attr('src', '/canciones/' + evento.data.fileIndex);
             document.getElementsByClassName('cancion-actual')[0].innerHTML = '<marquee>' + evento.data.nombre + '</marquee>';
             audio[0].play();
             
@@ -97,11 +98,14 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
 
         function cambiarCarpeta(evento){
             console.log('cambio carpeta: ', evento.currentTarget.innerText);
-            cargarCanciones(evento.currentTarget.innerText);
+            rutaActual += '\\' + evento.currentTarget.innerText;
+            cargarCanciones();
         }
 
         function subir(){
-            window.location.href = 'http://'+ window.location.host + '/subir';
+            console.log(rutaActual, ' ', rutaActual.substring(0,rutaActual.lastIndexOf('\\')));
+            rutaActual = rutaActual.substring(0,rutaActual.lastIndexOf('\\'));
+            cargarCanciones();
         }
 
         cargarCanciones('');
@@ -117,7 +121,7 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
                     elemActual.style.color = "#dfe0e6";
                     //console.log(indice);
                     audio[0].pause();
-                    audio.attr('src', '/canciones/' + arrayCanciones[indice].nombre);
+                    audio.attr('src', '/canciones/' + arrayCanciones[indice].fileIndex);
                     document.getElementsByClassName('cancion-actual')[0].innerHTML = '<marquee>' + arrayCanciones[indice].nombre + '</marquee>';
                     audio[0].play();
                     elemActual = document.getElementsByClassName('cancion')[indice];
@@ -133,7 +137,7 @@ $(function () { //Esta función se ejecuta al cargar el DOM (sintaxis de jquery)
                 elemActual.style.color = "#dfe0e6";
                 //console.log(indice);
                 audio[0].pause();
-                audio.attr('src', '/canciones/' + arrayCanciones[indice].nombre);
+                audio.attr('src', '/canciones/' + arrayCanciones[indice].fileIndex);
                 document.getElementsByClassName('cancion-actual')[0].innerHTML = '<marquee>' + arrayCanciones[indice].nombre + '</marquee>';
                 audio[0].play();
                 elemActual = document.getElementsByClassName('cancion')[indice];
